aed35dcd95cabf5a900b06cf1c76e8732498457d
미플 <devmeeple@gmail.com>
2024-07-15 13:10:19 +0900

    refactor: 입력 클래스 추가 및 분리

--- src/main/java/com/example/CalculatorMain.java
+++ src/main/java/com/example/CalculatorMain.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.util.Scanner;
-
 /**
  * 1. 값을 입력받는다.
  * 2. 값을 저장한다.
@@ -11,11 +9,7 @@ import java.util.Scanner;
 public class CalculatorMain {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("2개의 숫자와 연산자를 입력하세요 (e.g 1 + 2): ");
-        String result = scanner.nextLine();
-
-        String[] parts = result.split(" ");
+        String[] parts = new CalculationRequestReader().read();
 
         String operator = parts[1];
         long num1 = Long.parseLong(parts[0]);
