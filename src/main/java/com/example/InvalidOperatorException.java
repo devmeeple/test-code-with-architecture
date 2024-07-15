e14e338e0771767246f8865adf93852ad89a5c9a
미플 <devmeeple@gmail.com>
2024-07-15 12:23:22 +0900

    feat: 간단한 계산기 구현

---
+++ src/main/java/com/example/InvalidOperatorException.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class InvalidOperatorException extends RuntimeException {
+    public InvalidOperatorException() {
+        super("잘못된 연산자 입니다. 다음 4개의 연산자 중 하나를 입력해주세요. (+, -, *, /)");
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,33 @@
 package com.example;
 
+import java.util.Scanner;
+
+/**
+ * 1. 값을 입력받는다.
+ * 2. 값을 저장한다.
+ * 3. 입력 받은 값을 공백으로 분리한다.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("2개의 숫자와 연산자를 입력하세요 (e.g 1 + 2): ");
+        String result = scanner.nextLine();
+
+        String[] parts = result.split(" ");
+
+        String operator = parts[1];
+        long num1 = Long.parseLong(parts[0]);
+        long num2 = Long.parseLong(parts[2]);
+
+        long answer = switch (operator) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new InvalidOperatorException();
+        };
+        System.out.println(answer);
     }
 }
