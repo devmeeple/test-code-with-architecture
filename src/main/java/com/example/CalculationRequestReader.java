318b4cd5b103fc7e65fd91c53be3c3da77a59553
미플 <devmeeple@gmail.com>
2024-07-15 16:54:51 +0900

    refactor: 데이터 구조화

--- src/main/java/com/example/CalculationRequestReader.java
+++ src/main/java/com/example/CalculationRequestReader.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class CalculationRequestReader {
 
-    public String[] read() {
+    public CalculationRequest read() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("2개의 숫자와 연산자를 입력하세요 (e.g 1 + 2): ");
         String result = scanner.nextLine();
 
-        return result.split(" ");
+        String[] parts = result.split(" ");
+        return new CalculationRequest(parts);
     }
 }
--- src/main/java/com/example/CalculatorMain.java
+++ src/main/java/com/example/CalculatorMain.java
@@ -9,13 +9,13 @@ package com.example;
 public class CalculatorMain {
     public static void main(String[] args) {
 
-        String[] parts = new CalculationRequestReader().read();
+        CalculationRequest calculationRequest = new CalculationRequestReader().read();
 
-        String operator = parts[1];
-        long num1 = Long.parseLong(parts[0]);
-        long num2 = Long.parseLong(parts[2]);
-
-        long answer = new Calculator().calculate(num1, operator, num2);
+        long answer = new Calculator().calculate(
+                calculationRequest.getNum1(),
+                calculationRequest.getOperator(),
+                calculationRequest.getNum2()
+        );
         System.out.println("계산 결과: " + answer);
 
     }
